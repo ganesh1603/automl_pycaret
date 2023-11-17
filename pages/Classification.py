ad73d22f8119ae5924f06a6987c6de86e8970eeb
GANESH KUMAR E <118152658+ganesh1603@users.noreply.github.com>
2023-11-17 16:16:56 +0530

    Update Classification.py

--- pages/Classification.py
+++ pages/Classification.py
@@ -1,53 +1,79 @@
 import streamlit as st
-import pycaret as py
-import streamlit as st
-import plotly.express as px
-from pycaret.classification import setup, compare_models, pull, save_model,predict_model,plot_model
 import pandas as pd
-import pandas_profiling
+from pycaret.classification import setup, compare_models, save_model, load_model, plot_model
+from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
-import os 
+import plotly.express as px
+import os
 
-if os.path.exists('./dataset.csv'): 
+# Check if the dataset exists and load it
+if os.path.exists('./dataset.csv'):
     df = pd.read_csv('dataset.csv', index_col=None)
 
-
-with st.sidebar: 
-
+# SIDEBAR
+with st.sidebar:
     st.title("Auto Mashan ML")
-    choice = st.radio("Navigation", ["Upload","Profiling","Modelling","confusion_matrix","Download"])
+    choice = st.radio("Navigation", ["Upload", "Profiling", "Modelling", "Custom Plot", "Download"])
     st.info("This project application helps you build and explore your data.")
 
+# UPLOAD CSV OR EXCEL FILE
 if choice == "Upload":
     st.title("Upload Your Dataset")
-    file = st.file_uploader("Upload Your Dataset",type=["csv","data"])
-    if file: 
-        df = pd.read_csv(file, index_col=None)
+    file = st.file_uploader("Upload Your Dataset", type=["csv", "data", "xls", "xlsx"])
+    if file:
+        # Check the file extension and load the dataset accordingly
+        file_extension = file.name.split('.')[-1].lower()
+        if file_extension in ['csv', 'data']:
+            df = pd.read_csv(file, index_col=None)
+        elif file_extension in ['xls', 'xlsx']:
+            df = pd.read_excel(file, index_col=None)
+        # Save the dataset to a CSV file for later use
         df.to_csv('dataset.csv', index=None)
         st.dataframe(df)
 
-if choice == "Profiling": 
+# EDA BY PANDAS PROFILING
+if choice == "Profiling":
     st.title("Exploratory Data Analysis")
-    profile_df = df.profile_report()
-    st_profile_report(profile_df)
+    if 'df' in locals():
+        profile = ProfileReport(df, explorative=True)
+        st_profile_report(profile)
+    else:
+        st.warning("Please upload data by selecting 'Upload'.")
 
+# MODELLING THE DATA
 if choice == "Modelling":
-    chosen_target = st.selectbox('Choose the Target Column', df.columns)
-    if st.button('Run Modelling'): 
-        setup(df, target=chosen_target)
-        setup_df = pull()
-        st.dataframe(setup_df)
-        st.toast("wait till the best model leaderboard appear",icon="😁")
-        best_model = compare_models()
-        compare_df = pull()
-        st.dataframe(compare_df)
-        save_model(best_model, 'best_model')
+    if 'df' in locals():
+        chosen_target = st.selectbox('Choose the Target Column', df.columns)
+        if st.button('Run Modelling'):
+            s = setup(df, target=chosen_target, silent=True, session_id=123)
+            best_model = compare_models()
+            save_model(best_model, 'best_model')
+            st.success("Best model saved successfully!")
+    else:
+        st.warning("Please upload data by selecting 'Upload'.")
 
-if choice=="confusion_matrix":
-    plot=plot_model(best_model, plot = 'confusion_matrix')
-    st.image(plot)
+# CUSTOM PLOT
+if choice == "Custom Plot":
+    if os.path.exists('best_model.pkl'):
+        best_model = load_model('best_model')
+        st.title("Model Evaluation Plots")
+        available_plots = ['auc', 'confusion_matrix', 'threshold', 'pr', 'error', 'class_report', 'boundary', 'learning', 'manifold', 'calibration', 'vc', 'dimension', 'feature', 'feature_all', 'parameter']
+        selected_plot = st.selectbox('Select Plot Type', available_plots)
+        if st.button('Generate Plot'):
+            plot = plot_model(best_model, plot=selected_plot, save=True)
+            st.pyplot(plot)
+    else:
+        st.warning("Please run modelling first by selecting 'Modelling'.")
 
-if choice == "Download": 
-    model_file_path = 'best_model.pkl'  # Check if this file path is correct
-    with open(model_file_path, 'rb') as f: 
-        st.download_button('Download Model', f, file_name="best_model.pkl")
+# DOWNLOADING MODEL
+if choice == "Download":
+    if os.path.exists('best_model.pkl'):
+        with open('best_model.pkl', 'rb') as f:
+            st.download_button('Download Model', f, file_name="best_model.pkl")
+    else:
+        st.warning("Please run modelling first by selecting 'Modelling'.")
+
+# Run the Streamlit application
+if __name__ == '__main__':
+    st.title("AutoML with Streamlit")
+    st.write("Navigate through the sidebar to upload data, perform EDA, model, plot, and download the model.")
